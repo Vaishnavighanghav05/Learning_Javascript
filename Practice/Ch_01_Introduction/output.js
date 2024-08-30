b5b0a05359f664a80612f61d18e9a8074f7f2e2d
Vaishnavighanghav05 <ghanghavvaishnavi@gmail.com>
2024-08-30 11:15:57 +0000

    prev

---
+++ Practice/Ch_01_Introduction/output.js
@@ -0,0 +1,14 @@
+// console.log("hello")
+
+// document.write("hello")
+
+// alert("hi")
+
+// document.getElementById("heading").innerHTML="hello  js "
+
+
+let name =  prompt("Enter name")
+alert("Welcome, "+name)
+
+console.log(name);
+
